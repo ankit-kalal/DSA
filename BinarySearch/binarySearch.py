2cff733efe56062d48dc8fa7d38e3d378e8be49d
ANKIT <ankit@192.168.1.8>
2023-05-08 22:12:35 +0530

    first java program

--- BinarySearch/binarySearch.py
+++ BinarySearch/binarySearch.py
@@ -1,29 +1,26 @@
-def binarySearch(arr,key):
+def binary_search(arr, key):
     start = 0
     end = len(arr)-1
     mid = (start+end)//2
 
-    while start<=end:
-        if arr[mid]==key:
+    while start <= end:
+        if arr[mid] == key:
             return mid
 
-        if arr[mid]>key:
+        if arr[mid] > key:
             end = mid-1
         else:
             start = mid+1
 
         mid = (start+end)//2
-    
+
     return -1
 
 
-
 if __name__ == '__main__':
-  even = [2,4,6,8,12,18]
-  odd = [3, 8, 11, 14, 16]
-  
-  evenIndex = binarySearch(even, 6)
-  print("Index of 6 is: ", evenIndex)
-  
-  oddIndex = binarySearch(odd, 14)
-  print("Index of 14 is: ", oddIndex)
+    even = [2, 4, 6, 8, 12, 18]
+    odd = [3, 8, 11, 14, 16]
+    evenIndex = binary_search(even, 6)
+    print("Index of 6 is: ", evenIndex)
+    oddIndex = binary_search(odd, 14)
+    print("Index of 14 is: ", oddIndex)
